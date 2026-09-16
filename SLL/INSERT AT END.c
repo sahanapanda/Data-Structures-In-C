7114e2a3597f47e010ef768b757fbe21732f9eff
Sahana Panda <sahana.panda@gmail.com>
2026-09-16 09:35:41 +0530

    Add function to insert node at the end of linked list

---
+++ SLL/INSERT AT END.c
@@ -0,0 +1,54 @@
+//Insert at End
+#include <stdio.h>
+#include <stdlib.h>
+struct node{ 
+    int data; 
+    struct node *next; 
+} *head = NULL, *temp, *new_node; 
+
+void create(){ 
+    new_node = (struct node *)malloc(sizeof(struct node)); 
+    printf("Enter element : "); 
+    scanf("%d", &new_node -> data); 
+    new_node -> next = NULL; 
+    if(head == NULL){ 
+        head = new_node; 
+        temp = new_node; } 
+    else{ 
+        temp->next = new_node; 
+        new_node -> next = NULL; 
+        temp = new_node; }
+}
+void display(){ 
+    temp = head; 
+    if(temp == NULL){ 
+        printf("List is empty"); } 
+    else { while(temp!=NULL){ 
+        printf("%d -> ", temp -> data); 
+        temp = temp -> next; } 
+    printf("NULL\n"); } 
+}
+void insert_end(){ 
+    temp = head; 
+    new_node = (struct node*) malloc (sizeof(struct node)); 
+    printf("Enter element to add at ending : "); 
+    scanf("%d", &new_node -> data); 
+    while(temp->next!=NULL){
+        temp = temp->next;}
+    temp -> next = new_node;
+    new_node -> next = NULL;
+} 
+int main(){ 
+    int n; 
+    printf("Enter number of elements you want to create : "); 
+    scanf("%d", &n); 
+    for(int i=0; i<n; i++){ 
+        create(); } 
+    printf("Linked List : "); 
+    display(); 
+    //Insert at end 
+    insert_end(); 
+    printf("Modified Linked List (after insertion at end): ");
+    display(); 
+    return 0;
+}
