e12d9b91278fc46beb2295b6417ebaeada4018de
Sahana Panda <sahana.panda@gmail.com>
2026-09-16 10:30:58 +0530

    Add delete at beginning functionality for linked list
    Implement functionality to delete the first element of a linked list.

---
+++ SLL/DELETE AT BEGINNING.c
@@ -0,0 +1,52 @@
+//Delete at beginning
+#include <stdio.h>
+#include <stdlib.h>
+struct node{ 
+    int data; 
+    struct node *next; 
+} *head = NULL, *temp, *new_node; 
+
+void create(){ 
+    new_node = (struct node *)malloc(sizeof(struct node)); 
+    printf("Enter element : "); 
+    scanf("%d", &new_node -> data); 
+    new_node -> next = NULL; 
+    if(head == NULL){ 
+        head = new_node; 
+        temp = new_node; } 
+    else{ 
+        temp->next = new_node; 
+        new_node -> next = NULL; 
+        temp = new_node; }
+}
+void display(){ 
+    temp = head; 
+    if(temp == NULL){ 
+        printf("List is empty"); } 
+    else { while(temp!=NULL){ 
+        printf("%d -> ", temp -> data); 
+        temp = temp -> next; } 
+    printf("NULL\n"); } 
+}
+void delete_begin(){ 
+    temp = head; 
+    int ele;
+    printf("Element to be deleted : %d ", temp->data);
+    head = temp->next;
+    temp -> next = NULL;
+    free(temp);
+}
+int main(){ 
+    int n; 
+    printf("Enter number of elements you want to create : "); 
+    scanf("%d", &n); 
+    for(int i=0; i<n; i++){ 
+        create(); } 
+    printf("Linked List : "); 
+    display(); 
+    //Delete at beginning 
+    delete_begin(); 
+    printf("Modified Linked List (after deleting from beginning): ");
+    display(); 
+    return 0;
+}
